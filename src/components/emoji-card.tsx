3ca039e7a106955223a114f875ca5d2ec96313cf
LIU CHIAWEI <doublecheap@gmail.com>
2025-01-20 19:40:40 +0900

    update emoji card size

--- src/components/emoji-card.tsx
+++ src/components/emoji-card.tsx
@@ -31,7 +31,7 @@ function Card({ emoji, hueA, hueB, index }: CardProps) {
         <div className="relative z-10 drop-shadow-md">
           {emoji}
         </div>
-        <h1 className="text-[320px] text-slate-200 font-bold absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-0 select-none">{index}</h1>
+        <h1 className="text-[260px] text-slate-200 font-bold absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-0 select-none">{index}</h1>
       </motion.div>
     </motion.div>
   );
